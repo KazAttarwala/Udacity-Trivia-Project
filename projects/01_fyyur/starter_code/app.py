0d755f6b76d6f49363507e75e9d7d1ccac5a568a
KazAttarwala <52388168+KazAttarwala@users.noreply.github.com>
2021-04-22 17:28:29 -0500

    starter code with updated models

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -12,6 +12,7 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -20,6 +21,7 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # TODO: connect to a local postgresql database
 
@@ -38,8 +40,23 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = (db.String(500))
+    upcoming_shows = db.relationship('Show', backref='venue', lazy=True)
+    past_shows = db.relationship('Show', backref='venue', lazy=True)
+    past_shows_count = db.Column(db.Integer)
+    upcoming_shows_count = db.Column(db.Integer)
+    
+
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+artists_to_shows__assoc_table = db.Table('artists_shows',
+  db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True),
+  db.Column('show_id', db.Integer, db.ForeignKey('Show.id'), primary_key=True)
+)
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -52,10 +69,31 @@ class Artist(db.Model):
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean)
+    seeking_description = (db.String(500))
+    upcoming_shows = db.relationship('Show', secondary=artists_to_shows__assoc_table,
+      backref=db.backref('artists', lazy=True))
+    past_shows = db.relationship('Show', secondary=artists_to_shows__assoc_table,
+      backref=db.backref('artists', lazy=True))
+    past_shows_count = db.Column(db.Integer)
+    upcoming_shows_count = db.Column(db.Integer)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+class Show(db.Model):
+  __tablename__ = 'Show'
+  id = db.Column(db.Integer, primary_key=True)
+  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+  venue_name = db.Column(db.String)
+  #artist_id = db.Column(db.Integer)
+  #artist_name = db.Column(db.String)
+  #artist_image_link = db.Column(db.String)
+  venue_image_link = db.Column(db.String)
+  start_time = db.Column(db.DateTime)
+
 
 #----------------------------------------------------------------------------#
 # Filters.
