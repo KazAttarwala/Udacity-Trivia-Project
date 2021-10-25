09ab6fab44cb38ea3d9504789ba4c4c09c32dbf2
Qais Attarwala <qattarwala@sylvanroad.com>
2021-10-24 22:29:29 -0500

    moved code from __init__.py to app.py and updated test_flaskr and Procfile to reference app.py

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -3,7 +3,7 @@ import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
 
-from flaskr import create_app
+from flaskr.app import create_app
 from models import setup_db, Question, Category
 
 
